ac52e7ef34b14746a5a0254eccef679e2d23d050
Ryan Kadwell <ryan@pause.ca>
2013-06-12 12:35:15 -0400

    Added tests for beatLast and convertMove

--- Spec.hs
+++ Spec.hs
@@ -19,7 +19,30 @@ main = hspec $ do
     describe "RockPaperScissors.hs" $ do
         it "rock beats scissors" $ do
             outcome Rock Scissors `shouldBe` (1 :: Int)
+
         it "scissors beats paper" $ do
             outcome Scissors Paper `shouldBe` (1 :: Int)
+
         it "paper beats rock" $ do
             outcome Paper Rock `shouldBe` (1 :: Int)
+
+        it "beatlast knows what to play to beat rock" $ do
+            beatLast [Rock, Paper, Scissors] `shouldBe` Paper
+
+        it "beatlast knows what to play to beat paper" $ do
+            beatLast [Paper, Rock, Scissors] `shouldBe` Scissors
+
+        it "beatlast knows what to play to beat paper" $ do
+            beatLast [Scissors, Rock, Paper] `shouldBe` Rock
+
+        it "converts r and R to Rock" $ do
+            convertMove 'r' `shouldBe` Rock
+            convertMove 'R' `shouldBe` Rock
+
+        it "converts p and P to Paper" $ do
+            convertMove 'p' `shouldBe` Paper
+            convertMove 'P' `shouldBe` Paper
+
+        it "converts s and S to Scissors" $ do
+            convertMove 's' `shouldBe` Scissors
+            convertMove 'S' `shouldBe` Scissors
